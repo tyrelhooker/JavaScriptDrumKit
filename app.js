592c3f06d35e5ba07214daeeaef93d77bc0e26d0
tyrelhooker <TyrelHooker@gmail.com>
2018-10-09 16:10:31 -0400

    debug to find missing audio.play for sound

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 document.addEventListener('keydown', (event) => {
-  // selects audio data key for sound
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`); 
-  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-  console.log(key);
-  if(!audio) return; // stop the function from running
-  audio.currentTime = 0; // Allows key press multi sound
-  key.classList.add('playing');
+  console.log(event.keyCode);
+  // if (`keys[data-key="${event.keycode}"`) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`); // selects audio data key for sound
+  // }
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    console.log(audio);
+    if(!audio) return; // stop the function from running
+    audio.currentTime = 0; // Allows key press multi sound
+    audio.play(audio);
+    key.classList.add('playing');
 });
 
-function removeTransition(event) {
-  console.log(event);
-}
-
-// const keys = document.querySelectorAll('.key');
-// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
