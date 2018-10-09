9b7ded1adf997e76ddf4e15c7803953e6cbba826
tyrelhooker <TyrelHooker@gmail.com>
2018-10-09 16:33:27 -0400

    working JS drum kit with transitions on keys

---
+++ README.md
@@ -0,0 +1,9 @@
+JavaScript Drum Kit
+by: T.J. Hooker
+
+*Created using Wes Bos - JavaScript 30 tutorial*
+
+A keyboard interface that links each letter to a sound and key on keyboard.
+Press the corresponding key to emulate a drum sound. 
+
+Practicing keyEvents, playing audio, listening transitionEnd event.
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener('keydown', (event) => {
+function playSound(event) {
   console.log(event.keyCode);
   // if (`keys[data-key="${event.keycode}"`) {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`); // selects audio data key for sound
@@ -9,5 +9,15 @@ document.addEventListener('keydown', (event) => {
     audio.currentTime = 0; // Allows key press multi sound
     audio.play(audio);
     key.classList.add('playing');
-});
+};
 
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+// Listen on each key to end of transition
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)); 
+window.addEventListener('keydown', playSound);
